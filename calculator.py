068c00de468fb6ce2408504dde270da0a8112b44
anish.abraham <idofanish@gmail.com>
2024-01-29 20:46:46 +0530

    Added the development plan in Calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 #Calculator application code
 ##------------------------##
 
-#Section 1.1
+#Section 1.1 Addition
 
-#Section 1.2
+#Section 1.2 Subtraction
 
-#Section 1.3
+#Section 1.3 Multiplication
 
-#Section 1.4
+#Section 1.4 Division
