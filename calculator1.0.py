83f43720fa40df8846693fc6d6d06cbde3aaf079
anish.abraham <idofanish@gmail.com>
2024-01-30 20:42:56 +0530

    Release V1.0 completed on main

---
+++ calculator1.0.py
@@ -0,0 +1,15 @@
+#Calculator application code
+##------------------------##
+
+#Section 1.1 Addition
+#------Addition Logic starts here from Dev branch--------------
+ input(x,y)
+ s=x+y
+ print(s)
+ #Relv1.0 bugFix1,BugFix2 
+ #------End of Addition Logic-----------------
+#Section 1.2 Subtraction
+
+#Section 1.3 Multiplication
+
+#Section 1.4 Division
